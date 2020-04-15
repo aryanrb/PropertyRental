b8ff0ef2da7fbce9cbfe48687cee47216017a9ac
Aryan Rajbhandari <rajbhandari.aryanrb@gmail.com>
2020-04-15 13:39:43 +0545

    Layouts view added

---
+++ PropertyRental/resources/views/_layouts/customFoot.blade.php
@@ -0,0 +1,11 @@
+<!--====== Javascripts & Jquery ======-->
+{{-- <script src="{{ asset('cus/singlepage/js/jquery-3.2.1.min.js') }}"></script> --}}
+
+{{-- <script src="{{ asset('cus/singlepage/js/js/owl.carousel.min.js') }}"></script> --}}
+{{-- <script src="{{ asset('cus/singlepage/js/js/masonry.pkgd.min.js') }}"></script> --}}
+{{-- <script src="{{ asset('cus/singlepage/js/js/magnific-popup.min.js') }}"></script> --}}
+{{-- <script src="{{ asset('cus/singlepage/js/js/main.js') }}"></script> --}}
+
+{{-- <script src="{{ asset('cus/singlepage/js/bootstrap.min.js') }}"></script>  --}}
+
+<!-- load for map -->
